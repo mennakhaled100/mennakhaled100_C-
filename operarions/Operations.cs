ec7dde6872b611b127f5ee5c2597109f563f3e63
Mennat Allah Khaled <notakhaled46@gmail.com>
2025-07-12 16:29:21 +0300

    Add files via upload

---
+++ operarions/Operations.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4_part1
+{
+    static class Operations
+    {
+        //sum
+        public static int sum(int[] arr , int size)
+        {
+            int sum =0 ;
+            for( int i=0; i<size; i++)
+            {
+                sum += arr[i] ;
+            }
+            return sum ;    
+        }
+        //max
+        public static int max(int[] arr, int size)
+        {
+            int max = arr[0];
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] > arr[0])
+                    max = arr[i];
+            }
+            return max;
+        }
+        //min
+        public static int min(int[] arr, int size)
+        {
+            int min = arr[0];
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] < arr[0])
+                    min = arr[i];
+            }
+            return min;
+        }
+        //countPositive
+        public static int countPositive(int [] arr, int size)
+        {
+            int countPositive = 0;
+            for(int i = 0; i<size; i++)
+            {
+                if (arr[i] > 0)
+                    countPositive++;
+            }
+            return countPositive;
+        }
+        //countNegative
+        public static int countNegative(int[] arr, int size)
+        {
+            int countNegative = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] < 0)
+                    countNegative++;
+            }
+            return countNegative;
+        }
+        //countEven
+        public static int countEven(int[] arr, int size)
+        {
+            int countEven = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] % 2 ==0)
+                    countEven++;
+            }
+            return countEven;
+        }
+        //countOdd
+        public static int countOdd(int[] arr, int size)
+        {
+            int countOdd = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] % 2 != 0)
+                    countOdd++;
+            }
+            return countOdd;
+        }
+        
+
+    }
+}
---
+++ operarions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4_part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter arr size");
+           int size = int.Parse( Console.ReadLine());
+            Console.WriteLine("Enter arr values");
+            int[] arr = new int[size];
+            for(int i=0; i<size; i++)
+            {
+                arr[i] =    int .Parse(Console.ReadLine());
+            }
+
+            int sum = Operations.sum(arr , size);
+            int min = Operations.min(arr , size);
+            int max = Operations.max(arr, size);
+            int positive = Operations.countPositive(arr, size);
+            int negative = Operations.countNegative(arr, size);
+            int even = Operations.countEven(arr, size);
+            int odd = Operations.countOdd(arr, size);
+
+            Console.WriteLine("operations on arr: ");
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Min: " + min);
+            Console.WriteLine("Max: " + max);
+            Console.WriteLine("Positive: " + positive);
+            Console.WriteLine("Negative: " + negative);
+            Console.WriteLine("Even: " + even);
+            Console.WriteLine("Odd: " + odd);
+        }
+    }
+}
