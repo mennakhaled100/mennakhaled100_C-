06c9ce4e5cb805ee52a2216c9de9de3c90dbd6de
Mennat Allah Khaled <notakhaled46@gmail.com>
2025-08-04 22:44:19 +0300

    Add files via upload

---
+++ sheetAsiut/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace problemB
+{
+    internal class Program
+    {
+        static void Main(string[] arg)
+        {
+            string [] parts = Console.ReadLine().Split(' ');
+            int x = int.Parse(parts[0]);
+            long l = long.Parse(parts[1]);
+            char c = char.Parse(parts[2]);
+            float f = float.Parse(parts[3]);
+            double d = double.Parse(parts[4]);
+
+            Console.WriteLine(x);
+            Console.WriteLine(l);
+            Console.WriteLine(c);
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+        }
+    }
+}
